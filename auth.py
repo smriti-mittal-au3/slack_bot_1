415d1e1084c5cc4b45d03d352c1d6e16d4f95897
Smriti Mittal <mittal.smriti001@gmail.com>
2018-07-07 21:01:08 +0530

    Initial Commit for the hello bot

---
+++ auth.py
@@ -0,0 +1,97 @@
+
+
+import os
+from slackclient import SlackClient
+import pytz
+import datetime
+
+CLIENT_ID = os.environ.get(CLIENT_ID) 
+CLIENT_SECRET = os.environ.get(CLIENT_SECRET) 
+VERIFICATION_TOKEN = os.environ.get(VERIFICATION_TOKEN) 
+
+#authed_teams = {}
+
+timezones = []
+t = list(filter(lambda x: 'Asia' in x, pytz.all_timezones))
+for zone in t:
+    info = {
+        "text": zone,
+        "value": zone,
+    }
+    timezones.append(info)
+
+# A Dictionary of message attachment options
+attachments_json = [
+    {
+        "fallback": "What is your timezone?",
+        "color": "#3AA3E3",
+        "attachment_type": "default",
+        "callback_id": "timezone options",
+        "actions": [
+            {
+                "name": "timezone_list",
+                "text": "Select your timezone.",
+                "type": "select",
+                "options": timezones
+            }
+        ]
+    }
+]
+
+class Bot(object):
+    """ Instanciates a Bot object to handle Slack onboarding interactions."""
+    def __init__(self):
+        super(Bot, self).__init__()
+        self.name = "hello_bot"
+        self.emoji = ":robot_face:"
+        # When we instantiate a new bot object, we can access the app
+        # credentials we set earlier in our local development environment.
+        self.oauth = {"client_id": CLIENT_ID,
+                      "client_secret": CLIENT_SECRET,
+                      # Scopes provide and limit permissions to what our app
+                      # can access. It's important to use the most restricted
+                      # scope that your app will need.
+                      "scope": "bot"}
+        self.verification = VERIFICATION_TOKEN
+        self.client = SlackClient("")
+        # We'll use this dictionary to store the state of each message object.
+        # In a production envrionment you'll likely want to store this more
+        # persistantly in  a database.
+        self.messages = {}
+
+    def auth(self, code):
+        """Authenticate with OAuth and assign correct scopes.
+        """
+        auth_response = self.client.api_call(
+                                "oauth.access",
+                                client_id=self.oauth["client_id"],
+                                client_secret=self.oauth["client_secret"],
+                                code=code
+                                )
+        # To keep track of authorized teams and their associated OAuth tokens,
+        # we will save the team ID and bot tokens to the global
+        # authed_teams object
+        
+        team_id = auth_response["team_id"]
+        # authed_teams[team_id] = {"bot_token":
+        #                          auth_response["bot"]["bot_access_token"]}
+		bot_token = auth_response["bot"]["bot_access_token"]
+        # Then we'll reconnect to the Slack Client with the correct team's
+        # bot token
+        #self.client = SlackClient(authed_teams[team_id]["bot_token"])
+        self.client = SlackClient(bot_token)
+        #user_id = self.client.api_call("auth.test")["user_id"]
+        self.message("What is your timezone, please?",attachments_json)
+       
+
+    def message(self, text, attachments):
+    	"""send message to slack"""
+    	self.client.api_call(
+                      "chat.postMessage",
+                      channel="#general",
+                      text=text,
+                      attachments=attachments
+                        )
+
+
+        
---
+++ new.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, make_response, Response, render_template
+import os
+import json
+import pytz
+import datetime
+import time
+import re
+import auth
+from slackclient import SlackClient
+
+#SLACK_WEBHOOK_SECRET ="9OXQcUS9PqzdAvyh7E6KWqP6"
+DELAY = 1 #86400 #24 hours
+starterbot_id = None # starterbot's user ID in Slack: value is assigned after the bot starts up
+
+pyBot = auth.Bot()
+slack_client = pyBot.client
+
+app = Flask(__name__)
+
+#The endpoint Slack will load your menu options from
+@app.route("/options", methods=["POST"])
+def message_options():
+    # Parse the request payload
+    form_json = json.loads(request.form["payload"])
+    # Dictionary of menu options which will be sent as JSON
+    menu_options = {"options": timezones}
+    # Load options dict as JSON and respond to Slack
+    return Response(json.dumps(menu_options), mimetype='application/json')
+
+
+@app.route("/install", methods=["GET"])
+def pre_install():
+    """This route renders the installation page with 'Add to Slack' button."""
+    # Since we've set the client ID and scope on our Bot object, we can change
+    # them more easily while we're developing our app.
+    client_id   = pyBot.oauth["client_id"]
+    scope       = pyBot.oauth["scope"]
+    return render_template("install.html", client_id=client_id, scope=scope)
+
+
+@app.route("/thanks", methods=["GET", "POST"])
+def thanks():
+    """"""
+    code_arg = request.args.get('code')
+    # The bot's auth method to handles exchanging the code for an OAuth token
+    
+    print(code_arg)
+    pyBot.auth(code_arg)                  
+    return make_response("Thank you! You have successfully installed the bot"), 200
+
+
+# The endpoint Slack will send the user's menu selection to
+@app.route("/slack", methods=["POST"])
+def message_actions():
+    """send "hi" at 12pm of the selected timezone"""
+    print(request)
+    print(request.form)
+    form_json = json.loads(request.form["payload"])
+    selection = form_json["actions"][0]["selected_options"][0]["value"]
+
+    intz = pytz.timezone(selection)
+    nowdt = datetime.datetime.now(intz)
+    nowt = nowdt.strftime('%H:%M:%S')
+    midday=datetime.time(hour=11, minute=59,second=59,tzinfo=intz).strftime('%H:%M:%S')
+    
+    while True:   
+        if(nowt==midday):
+            pyBot.message("hi", [])
+        time.sleep(DELAY)
+
+    return make_response(""),200
+
+
+
+# Start the Flask server
+if __name__ == "__main__":
+    app.run(debug=False)
